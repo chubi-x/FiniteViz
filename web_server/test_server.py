75f114b7bfa270cd8cf536f61f4976d6d915f8c5
chubix <chubixax@protonmail.com>
2023-07-18 15:35:24 +0100

    feat: flask server for publishing tasks

--- web_server/test_server.py
+++ web_server/test_server.py
@@ -1,46 +1,51 @@
-import uuid
-from flask import Flask, request, jsonify
-import pika
+from flask import Flask, request, jsonify, send_file
 import redis
+from enum import Enum
+import uuid
 import json
 
+from publisher import MessagePublisher
+import ResponseHandler
+
 REDIS_HOST = "172.17.0.4"
-RABBIT_HOST = "172.17.0.3"
 pool = redis.ConnectionPool(host=REDIS_HOST, port=6379, db=0)
 redis_store = redis.Redis(connection_pool=pool, decode_responses=True)
-rabbit_connection = pika.BlockingConnection(pika.ConnectionParameters(RABBIT_HOST))
-
-
-def send_message(message):
-    channel = rabbit_connection.channel()
-    channel.queue_declare(queue="request")
-    channel.basic_publish(
-        exchange="",
-        routing_key="request",
-        properties=pika.BasicProperties(content_type="application/json"),
-        body=json.dumps(message),
-    )
-    channel.close()
-
 
 app = Flask(__name__)
+
+publisher = MessagePublisher()
+
+
+class MESSAGE_STATUS(Enum):
+    PROCESSING = "processing"
+    FAILED = "failed"
+    SUCCESS = "success"
+    RETRY = "retry"
+
+
+EMPTY_TASK = {"success": False, "data": {"message": "Task does not exist"}}
 
 
 @app.route("/message", methods=["POST"])
 def message():
-    # save to redis
-    # check if task exists
     task_id = str(uuid.uuid4())
-    mapping = {"status": "processing", "result": None, "success": False}
-    redis_store.set(task_id, json.dumps(mapping))
     message = {"id": task_id, "payload": request.get_json()}
-    send_message(message)
-    return jsonify({"task_id": task_id, "status": "processing"})
+    message_sent, err = publisher.send_message(message)
+    if message_sent:
+        return ResponseHandler.task_creation_success(payload=message)
+    else:
+        if err:
+            return ResponseHandler.task_creation_error(payload=message, message=err)
+        else:
+            return ResponseHandler.task_creation_error(payload=message)
 
 
 @app.get("/poll/<id>")
 def poll(id: str):
-    result = redis_store.get(id)
+    try:
+        result = redis_store.get(id)
+    except Exception as err:
+        return ResponseHandler.server_error(message=str(err))
     if result is not None:
-        return json.loads(result)
-    return "Not found"
+        return ResponseHandler.task_success(payload=json.loads(result))
+    return ResponseHandler.empty_task()
