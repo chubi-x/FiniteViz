8648b48a611b852ad645cf96705f88670e509842
chubix <chubixax@protonmail.com>
2023-08-03 19:46:25 +0100

    gotten as far as mesh generation. Getting segfault so data isn't being parsed correctly.

--- compute_worker/mpap3_chubi/proj/smsh/consumer.h
+++ compute_worker/mpap3_chubi/proj/smsh/consumer.h
@@ -60,6 +60,61 @@ private:
 	};
 	const char *MessageStatus[2] = {"SUCCESS", "FAILED"};
 
+	class VectorHelpers
+	{
+	public:
+		/// @brief Print a vector of vectors
+		/// @tparam T
+		/// @param property
+		/// @param propertyName
+		template <typename T>
+		static void print(std::vector<std::vector<T>> property, string propertyName = "")
+		{
+			std::cout << propertyName << ": [";
+			for (std::vector<T> const row : property)
+			{
+				cout << "[";
+				std::copy(row.begin(),
+						  row.end(),
+						  std::ostream_iterator<T>(std::cout, ", "));
+				cout << "]";
+			}
+			std::cout << "] " << endl;
+		}
+		/// @brief Template function to flatten a vector of vectors
+		/// @tparam T
+		/// @param input
+		/// @return
+		template <typename T>
+		static std::vector<T> flatten(const std::vector<std::vector<T>> &input)
+		{
+			cout << "Flattening vector of vectors" << endl;
+			print(input, "vector to be flattened:");
+			size_t totalSize = 0;
+			for (const auto &innerVector : input)
+			{
+				totalSize += innerVector.size();
+			}
+
+			std::vector<T> flattened;
+			flattened.reserve(totalSize);
+			for (const auto &innerVector : input)
+			{
+				flattened.insert(flattened.end(), innerVector.begin(), innerVector.end());
+			}
+
+			cout << "Flattened vector:" << endl;
+			cout << "[";
+			for (T const row : flattened)
+			{
+				cout << row << " , ";
+			}
+			cout << "]";
+
+			return flattened;
+		}
+	};
+
 	struct Amqp
 	{
 		AmqpClient::Channel::ptr_t channel;
@@ -79,45 +134,49 @@ private:
 
 	struct Mesh
 	{
-		int num_nodes;
-		int num_dims;
-		int num_elements;
-		int nodes_per_element;
-		std::vector<std::vector<float>> coordinates;
+		std::size_t num_nodes;
+		std::size_t num_dims;
+		std::size_t num_elements;
+		std::size_t nodes_per_element;
+		// int num_nodes;
+		// int num_dims;
+		// int num_elements;
+		// int nodes_per_element;
+		std::vector<std::vector<double>> coordinates;
 		std::vector<std::vector<int>> connectivities;
 		std::vector<std::vector<int>> splitting;
 
+		/// @brief Template function to set the properties of the mesh
+		/// @tparam T
+		/// @param property
+		/// @param property_name
+		/// @param mesh_property
+		/// @param mesh_rows
+		/// @param mesh_cols
 		template <typename T>
-		void set_properties(const json &property, const string &property_name, std::vector<std::vector<T>> &mesh_property, const int &mesh_rows = 0, const int &mesh_cols = 0)
+		// void set_properties(const json &property, const string &property_name, std::vector<std::vector<T>> &mesh_property, int *mesh_rows = 0, int *mesh_cols = 0)
+		void set_properties(const json &property, const string &property_name, std::vector<std::vector<T>> &mesh_property, std::size_t *mesh_rows, std::size_t *mesh_cols)
 		{
 			property.get_to(mesh_property);
 			if (property_name != "splitting")
 			{
-				mesh_rows = property.size();
-				mesh_cols = property[0].size();
+				*mesh_rows = property.size();
+				*mesh_cols = property[0].size();
 			}
-		}
-		template <typename T>
-		void print(std::vector<std::vector<T>> property, string propertyName)
-		{
-			std::cout << propertyName << ": [";
-			for (std::vector<std::vector<T>> const &row : property)
-			{
-				cout << "[";
-				std::copy(row.begin(),
-						  row.end(),
-						  std::ostream_iterator<T>(std::cout, ", "));
-				cout << "]";
-			}
-			std::cout << "] " << endl;
 		}
 	};
+	/// @brief Mesh json mapping
 	const unordered_map<string, string> mesh_json_mapping = {
 		{"splitting", "splitting"},
 		{"coordinates", "coordinates"},
 		{"connectivities", "connectivities"},
 	};
 
+	/// @brief Parse the json payload to a mesh object
+	/// @param mesh
+	/// @param task_id
+	/// @param payload
+	/// @return
 	bool parseJsonToMesh(Mesh &mesh, string &task_id, const json &payload)
 	{
 		bool valid_json = true;
@@ -133,11 +192,11 @@ private:
 				try
 				{
 					if (memberName == "coordinates")
-						mesh.set_properties(propertyValue, memberName, mesh.coordinates, mesh.num_nodes, mesh.num_dims);
+						mesh.set_properties(propertyValue, memberName, mesh.coordinates, &mesh.num_nodes, &mesh.num_dims);
 					else if (memberName == "connectivities")
-						mesh.set_properties(propertyValue, memberName, mesh.connectivities, mesh.num_elements, mesh.nodes_per_element);
+						mesh.set_properties(propertyValue, memberName, mesh.connectivities, &mesh.num_elements, &mesh.nodes_per_element);
 					else if (memberName == "splitting")
-						mesh.set_properties(propertyValue, memberName, mesh.splitting);
+						mesh.set_properties(propertyValue, memberName, mesh.splitting, 0, 0);
 					else
 					{
 						valid_json = false;
@@ -201,15 +260,56 @@ public:
 			BOOST_LOG_TRIVIAL(error) << "Error sending results: " << e.what();
 		}
 	}
-	void generate_mesh_elements(FemStdMesh &fem, Mesh &mesh)
+	void generate_mesh_elements(FemStdMesh &fem, Mesh mesh)
 	{
-		const char *mesh_type = mesh.num_nodes == 2 ? "mesh2D" : "mesh3D";
-		// int ix1[(2 + 1) * 3];
-		// int ix1[2];
+		const char *mesh_type = mesh.num_dims == 2 ? "mesh2D" : "mesh3D";
+		int ix1[mesh.nodes_per_element * mesh.num_elements];
+		int *connectivities = VectorHelpers::flatten(mesh.connectivities).data();
+		// fill ix1 with the data from connectivities
+		for (size_t i = 0; i < mesh.nodes_per_element * mesh.num_elements; i++)
+		{
+			ix1[i] = connectivities[i];
+			cout << "Ix: " << ix1[i] << "Connectivity: " << connectivities[i] << endl;
+		}
+		double *coordinates = VectorHelpers::flatten(mesh.coordinates).data();
+		cout << "Generating mesh elements..." << endl;
+		try
+		{
+			cout << "Nen: " << mesh.nodes_per_element << endl;
+			for (size_t e = 0; e < mesh.num_elements; e++)
+				fem.generateElement(mesh_type, ix1 + e * (mesh.nodes_per_element), mesh.nodes_per_element - 1);
+		}
+		catch (const std::exception &e)
+		{
+			BOOST_LOG_TRIVIAL(error) << "Error generating mesh elements: " << e.what();
+		}
+		cout << "Mesh elements generated successfully!" << endl;
+		cout << "Generating mesh nodes..." << endl;
+		fem.x.setDim(mesh.num_dims * mesh.nodes_per_element);
+		cout << "Mesh nodes generated successfully!" << endl;
+		// set coordinates
+		cout << "Setting coordinates..." << endl;
+		for (size_t n = 0; n < sizeof(coordinates); n++)
+			fem.x[n] = coordinates[n];
+		cout << "Coordinates set successfully!" << endl;
+		// perform splitting
+		cout << "Performing splitting..." << endl;
+		// mesh.print(mesh.splitting);
+		for (const auto &node : mesh.splitting)
+		{
+			cout << "Node: " << node[0] << " " << node[1] << " " << node[2] << endl;
+			fem.split(node[0], node[1], node[2]);
+		}
 
-		for (int e = 0; e < mesh.num_elements; e++)
-			fem.generateElement(mesh_type, ix1 + e * (mesh.nodes_per_element + 1), mesh.nodes_per_element);
+		cout << "Splitting performed successfully!" << endl;
+		cout << "Writing mesh to file..." << endl;
+		fem.writeMeshToFile("finiteViz.mesh");
+		cout << "Mesh written to file successfully!" << endl;
 	}
+	/// @brief Process message from AMQP
+	/// @param task_id
+	/// @param payload
+	/// @param envelope
 	void process_message(string &task_id, json &payload, const AmqpClient::Envelope::ptr_t &envelope)
 	{
 		Mesh mesh;
@@ -218,6 +318,7 @@ public:
 		if (!task_id.empty() && parsed)
 		{
 			FemStdMesh fem("mesh", mesh.num_dims);
+			generate_mesh_elements(fem, mesh);
 
 			// TODO: if success send results and acknowledge message
 			json payload = {
@@ -249,6 +350,8 @@ public:
 	{
 		amqp_members.channel->BasicAck(envelope);
 	}
+
+	/// @brief Begin consuming messages from the queue
 	void consume()
 	{
 		BOOST_LOG_TRIVIAL(info) << "Starting Consumer...";
@@ -270,7 +373,6 @@ public:
 			{
 				task_id = body["task_id"];
 			}
-			// catch error
 			catch (nlohmann::detail::type_error const &e)
 			{
 				BOOST_LOG_TRIVIAL(error) << "Provided invalid key 'task_id'" << e.what();
