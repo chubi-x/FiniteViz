55b12b67d1c891093cb1c24e73f364e420292e0b
chubix <chubixax@protonmail.com>
2023-09-18 17:53:08 +0100

    remove default element

--- client/finiteViz/src/Meshes/BaseMesh/Elements.jsx
+++ client/finiteViz/src/Meshes/BaseMesh/Elements.jsx
@@ -3,21 +3,26 @@ export default function Elements ({ state, numNodes, showSplits, styles }) {
   const { elements, baseMeshDispatch, nodesPerElement, numElements } = state
   const { buttonStyles } = styles
   // const defaultElements = [
+  //   // [0, 1, 2, 3, 1],
+  //   // [1, 4, 5, 2, 1],
+  //   // [3, 2, 5, 6, 1]
+  //   [0, 1, 2, 3, 4, 5, 6, 7]
+  // ]
   const initialElements = Array.from({ length: numElements }, () =>
     new Array(nodesPerElement).fill('')
   )
-
+  useEffect(() => {
+    if (elements.length === 0) {
+      baseMeshDispatch({
+        type: 'elements',
+        payload: initialElements
+      })
+    }
+  }, [])
   const checkElementsComplete = () => {
     return elements.every(el => el.every(node => node !== ''))
   }
   function updateElements (i, j, value) {
-    // setElements(prev => {
-    //   return prev.map((row, rowIndex) =>
-    //     rowIndex === i
-    //       ? [...row.slice(0, j), parseInt(value), ...row.slice(j + 1)]
-    //       : row
-    //   )
-    // })
     baseMeshDispatch({
       type: 'elements',
       payload: elements.map((row, rowIndex) =>
@@ -35,9 +40,7 @@ export default function Elements ({ state, numNodes, showSplits, styles }) {
   function defaultNodeValues (numElements) {
     const defaults = []
     for (let i = 0; i < nodesPerElement; i++) {
-      useDefaultElements
-        ? defaults.push(elements[numElements][i])
-        : defaults.push('Choose Node')
+      defaults.push('Choose Node')
     }
     return defaults
   }
@@ -55,11 +58,9 @@ export default function Elements ({ state, numNodes, showSplits, styles }) {
                 onChange={e => updateElements(i, j, e.target.value)}
                 key={j}
               >
-                {!useDefaultElements && (
-                  <option disabled value='Choose Node'>
-                    Choose Node
-                  </option>
-                )}
+                <option disabled value='Choose Node'>
+                  Choose Node
+                </option>
                 {printElementOptions().map(option => (
                   <option key={option} value={option}>
                     {' '}
@@ -95,15 +96,6 @@ export default function Elements ({ state, numNodes, showSplits, styles }) {
             } ${buttonStyles}`}
           >
             Define Splits
-          </button>
-          <button
-            id='vizBaseMesh'
-            onClick={e => vizBaseMesh(true)}
-            disabled={!checkElementsComplete()}
-            type='button'
-            className='bg-blue-300 p-2 rounded-md'
-          >
-            Visualise Base Mesh
           </button>
         </div>
       </form>
