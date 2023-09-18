69883d6b941d87d111666e9ab80ce4862b4512aa
chubix <chubixax@protonmail.com>
2023-09-18 17:27:20 +0100

    chore: signal handler for mesh generation fetch

--- client/finiteViz/src/api/fetch.js
+++ client/finiteViz/src/api/fetch.js
@@ -2,6 +2,7 @@ export async function generateMesh (body) {
   const response = await fetch('http://127.0.0.1:3000/message', {
     method: 'POST',
     body: JSON.stringify(body),
+    signal: AbortSignal.timeout(5000),
     mode: 'cors',
     headers: {
       'Content-type': 'application/json',
