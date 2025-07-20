1480f81970779d858d0776968e015468c00ca7b2
chubi-x <chubixax@protonmail.com>
2025-07-20 23:34:59 +0100

    create vite api proxy

--- client/finiteViz/src/api/fetch.js
+++ client/finiteViz/src/api/fetch.js
@@ -1,15 +1,15 @@
-export async function generateMesh (body) {
-  const response = await fetch(`${import.meta.env.VITE_API_URL}/message`, {
-    method: 'POST',
+export async function generateMesh(body) {
+  const response = await fetch(`api/message`, {
+    method: "POST",
     body: JSON.stringify(body),
     signal: AbortSignal.timeout(5000),
-    mode: 'cors',
+    mode: "cors",
     headers: {
-      'Content-type': 'application/json',
-      Accept: 'application/json',
-      'ngrok-skip-browser-warning': 'true'
-    }
-  })
-  return await response.json()
+      "Content-type": "application/json",
+      Accept: "application/json",
+      // "ngrok-skip-browser-warning": "true",
+    },
+  });
+  return await response.json();
 }
 // export async function pollNewMesh
--- client/finiteViz/vite.config.js
+++ client/finiteViz/vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  server: {
+    port: 3000,
+    strictPort: true,
+    host: true,
+    proxy: {
+      "/api": {
+        target: `http://${process.env.VITE_API_HOST}`,
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+});
