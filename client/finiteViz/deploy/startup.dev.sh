b05ee9e8a34be86582e9fa814f24e6319bb53e0d
chubi-x <chubixax@protonmail.com>
2025-07-20 23:30:05 +0100

    fix deployment scripts

---
+++ client/finiteViz/deploy/startup.dev.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+npm install serve -g
+npm install
+npm run dev
--- client/finiteViz/startup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-export $(grep -v '^#' .env | xargs)
-ngrok config add-authtoken $NGROK_TOKEN
-npm run preview &
-ngrok http --domain=pipefish-funny-ox.ngrok-free.app 3000
